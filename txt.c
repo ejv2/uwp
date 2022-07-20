9af57067fad24b4489d79b3acb08d6b54281ca2c
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2022-07-20 01:36:55 +0100

    txt: add html strip algorithm
    Runs a faux sliding window algorithm which will strip bytes between a
    '<' and '>' character by memmoving the contents past the boundary byte.
    There may be a more efficient method which does not involve so much
    copying, but this is by far the simplest method I could devise.

--- txt.c
+++ txt.c
@@ -1,8 +1,10 @@
 /* See LICENSE for copyright and license details. */
 
 #include <string.h>
-
 #include "txt.h"
+
+static const char html_open = '<';
+static const char html_close = '>';
 
 char *
 limit_string(const char *orig, long max)
@@ -16,4 +18,25 @@ limit_string(const char *orig, long max)
 
 	*inc = 0;
 	return dup;
+}
+
+void strip_html(char *buf)
+{
+	char *endptr;
+	char *ptr, *hbegin, *hend;
+
+	if (!buf)
+		return;
+
+	for (endptr = buf; *endptr; endptr++);
+	for (ptr = buf; *ptr; ptr++) {
+		if (*ptr == html_open)
+			hbegin = ptr;
+
+		if (*ptr == html_close) {
+			hend = ptr;
+			memmove(hbegin, hend+1, endptr-(hend)+1);
+			ptr = hbegin-1;
+		}
+	}
 }
--- txt.h
+++ txt.h
@@ -1,3 +1,4 @@
 /* See LICENSE for copyright and license details. */
 
 char *limit_string(const char *orig, long max);
+void strip_html(char *buf);
